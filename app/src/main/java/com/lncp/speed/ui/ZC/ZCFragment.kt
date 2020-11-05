fff7cb1099780142913356d5fb8f5b4dcbe065e5
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-11-05 16:28:09 +0800

    Update ZCFragment.kt

--- app/src/main/java/com/lncp/speed/ui/ZC/ZCFragment.kt
+++ app/src/main/java/com/lncp/speed/ui/ZC/ZCFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.lncp.speed.R
 import kotlinx.android.synthetic.main.fragment_zc.*
 import threeDvector.Vec3D
