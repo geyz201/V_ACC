d17c7311a53b72fdfa208a528b1f2696b397ead2
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-11-06 15:24:50 +0800

    申请权限

--- app/src/main/java/com/lncp/speed/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lncp/speed/ui/home/HomeFragment.kt
@@ -1,19 +1,26 @@
 package com.lncp.speed.ui.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.lncp.speed.R
+import com.lncp.speed.SensorRecord
 import com.lncp.speed.ServiceCheckUtil
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private val model: HomeViewModel by viewModels()
+
+    private val LOCATION_PERMISSION = 1
 
     var processState = false
 
@@ -43,6 +50,31 @@ class HomeFragment : Fragment() {
             } else {
                 //开启蓝牙
                 //getPairedDevices()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if (checkSelfPermission(requireContext(),Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        //requestPermissions是异步执行的
+                        requestPermissions(
+                            arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                            LOCATION_PERMISSION
+                        )
+                        while (checkSelfPermission(requireContext(),Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        }
+                    }
+                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        //requestPermissions是异步执行的
+                        requestPermissions(
+                            arrayOf(
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ),
+                            LOCATION_PERMISSION
+                        )
+                        while (checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        }
+                    }
+                }
 
                 val intent = Intent(requireContext(), SensorRecord::class.java)
                 intent.action = "com.lncp.server.SensorRecord"
